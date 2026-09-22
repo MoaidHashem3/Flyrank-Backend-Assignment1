f4115400c15a83edcc93c707b805fc09e46fa069
MoaidHashem3 <gamestorrent2015@gmail.com>
2026-09-22 14:13:11 +0300

    Stage 5: Swagger UI

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./openapi.json');
 const app = express();
 const port = 3000;
 app.use( express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // in memory data store
 const TASKS = [
